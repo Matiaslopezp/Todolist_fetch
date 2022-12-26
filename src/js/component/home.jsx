f1611ab73865c5f6b2d5c310f75cc28db5cf63a0
Matiaslopezp <lopezp.matias@gmail.com>
2022-12-26 10:43:18 +0000

    save final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,37 +3,71 @@ import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-const Css={
-  backgrabackgroundColor: "black",
-  width: "1000px",
-  height: "1000px",
+const bodyCss = {
+  backgroundColor: "#ea899a",
+  textAlign: "center",
+  marginTop: "100px",
+  paddingTop:"0px",
+  width:"500px",
+ 
+};
+
+
+
+const Css = {
+  backgroundColor: "#FF6666",
+  textAlign: "center",
+  marginTop: "2px",
+  paddingTop:"20px",
+  width:"500px",
+  textAlign:"center"
+};
+
+const Css2={
+  fontStyle: "italic",
+  marginTop:"15px"
 }
+
 
 //create your first component
 const Home = () => {
-  const [tarea, Settarea] = useState([ ]);
+  const [tarea, Settarea] = useState([]);
+  const borrar = (b) => {
+    Settarea(tarea.filter((value, index, arr) => {
+        return index != b;
+      })
+    );
+  };
+
   return (
-    <div className="text-center" style={Css}>
+    <div className="text-center container">
+      <div className="text-center container" style={bodyCss}>
       <h1>TODO List</h1>
-
-        <input placeholder="Ingresa su nueva Tarea" onKeyPress={(e)=>{
-          if(e.key==="Enter"){
-            Settarea([...tarea,e.target.value]);
-            e.target.value="";
+      </div>
+      <div className="container"style={Css}>
+      <input
+        placeholder="Ingresa una nueva Tarea"
+        onKeyPress={(e) => {
+          if (e.key === "Enter") {
+            Settarea([...tarea, e.target.value]);
+            e.target.value = "";
           }
-        }} />
-		
+        }}
+      />
 
-        {tarea.map((value, index, arr) => {
-          console.log(value, index, arr);
-          return (
-            <li key={index}>
-              {value}
-              <button>x</button>
-            </li>
-          );
-        })}
-      
+      {tarea.map((value, index, arr) => {
+        console.log(value, index, arr);
+        return (
+          <li key={index}>
+            {value}
+            <button onClick={() => borrar(index)}><i class="far fa-trash-alt"></i></button>
+          </li>
+        );
+      })}
+      <div style={Css2}>
+        Número de tareas {tarea.length}
+        </div>
+      </div>
     </div>
   );
 };
