28b551dc23fa32ec29921839791f50d95a5681a8
Matiaslopezp <lopezp.matias@gmail.com>
2022-12-27 20:55:22 +0000

    save0

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -9,10 +9,13 @@ const bodyCss = {
   marginTop: "100px",
   paddingTop:"0px",
   width:"500px",
- 
 };
 
-
+const listaCss={
+  display: "flex",
+  flexDirection: "column",
+  justifyContent:"start",
+};
 
 const Css = {
   backgroundColor: "#FF6666",
@@ -32,6 +35,7 @@ const Css2={
 //create your first component
 const Home = () => {
   const [tarea, Settarea] = useState([]);
+  
   const borrar = (b) => {
     Settarea(tarea.filter((value, index, arr) => {
         return index != b;
@@ -39,31 +43,80 @@ const Home = () => {
     );
   };
 
+  
+
+ //crear fetch metodo PUT
+ function putApi() {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  //cambia el arreglo en duro por la variable de estado
+  var raw = JSON.stringify(tarea);
+
+  var requestOptions = {
+    method: "PUT",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(
+    "https://assets.breatheco.de/apis/fake/todos/user/mlp",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+
+//-----------------------------------------------------------
+
+  useEffect(()=>{
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/mlp")
+    .then((response)=>response.json())
+    .then((data)=>Settarea(data));
+  },[]);
+
+ //-----------------------------------------------------------
+ useEffect(()=>{ 
+  putApi();
+ },[tarea]);
+// es l oque qeuda de la tarea
+ //-----------------------------------------------------------
+
+
   return (
     <div className="text-center container">
       <div className="text-center container" style={bodyCss}>
       <h1>TODO List</h1>
       </div>
       <div className="container"style={Css}>
-      <input
-        placeholder="Ingresa una nueva Tarea"
-        onKeyPress={(e) => {
-          if (e.key === "Enter") {
-            Settarea([...tarea, e.target.value]);
-            e.target.value = "";
-          }
+     
+      <form 
+        onSubmit={(e) => {
+          e.preventDefault(); //que no se actualize el componente//
+          Settarea([...tarea, { label: e.target[0].value, done: false }]);
+          putApi();
         }}
-      />
+        >
+        <input type="text" placeholder="Ingese la tarea"></input>
+        <button> Agregar </button>
+      </form>
+
 
       {tarea.map((value, index, arr) => {
         console.log(value, index, arr);
         return (
-          <li key={index}>
-            {value}
-            <button onClick={() => borrar(index)}><i class="far fa-trash-alt"></i></button>
+          <div style={listaCss}>
+          <li key={index} >
+            {value.label}
+            <button onClick={() => borrar(index)}><i className="far fa-trash-alt"></i></button>
           </li>
+        </div>
         );
       })}
+
+
       <div style={Css2}>
         Número de tareas {tarea.length}
         </div>
